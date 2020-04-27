69a7566b5d9a18293d617c889b167fa54eff6575
Kristers Lielups <kristers.lielups@gmail.com>
2020-04-27 21:32:55 +0300

    Implement basic file upload

--- Compressify/urls.py
+++ Compressify/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Compressor.views import home_view, result_view
+
 urlpatterns = [
+    path('', home_view, name='home'),
+    path('result_view', result_view, name='results'),
     path('admin/', admin.site.urls),
 ]
--- Compressor/views.py
+++ Compressor/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from .forms import ImageUploadForm
+from django.template import RequestContext
+from PIL import Image
+
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    if request.method == 'POST' and request.FILES.get('file', False):
+        form = ImageUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'], request.POST['title'])
+            return HttpResponseRedirect('/result_view')
+        return HttpResponseRedirect('/')
+    else:
+        form = ImageUploadForm()
+        return render(request, 'home.html', {'form': form})
+
+
+def result_view(request, *args, **kwargs):
+    files = {
+        'image_source': 'vvv',
+        'image_png': 'aaa',
+        'image_jpeg': 'bbb'
+    }
+    v = 4
+    return render(request, 'result_view.html', files)
+
+
+def handle_uploaded_file(f, name):
+    with open('files/source/' + name + '.bmp', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
